1d63359a3d695aad2c82d32c2f18fc24b7029215
Ivan Verdugo <ivan270@gmail.com>
2023-04-26 09:43:57 -0400

    added form reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 let form = document.querySelector('form');
+
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
 	let datosFormulario = capturarDatos();
 	generarAlert(datosFormulario);
+	form.reset();
 });
+
 let capturarDatos = () => {
 	let datos = { nombre: '', apellido: '', mail: '', motivo: '', mensaje: '' };
 	datos.nombre = usuarioNombre.value;
@@ -13,6 +16,7 @@ let capturarDatos = () => {
 	datos.mensaje = usuarioMensaje.value;
 	return datos;
 };
+
 let generarAlert = (datos) => {
 	alert(
 		`DE: ${datos.nombre} ${datos.apellido} (${datos.mail})\n- ASUNTO: ${datos.motivo}\n- MENSAJE: \n${datos.mensaje}.`
